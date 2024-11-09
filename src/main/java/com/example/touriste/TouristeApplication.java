d6708cb774629116c7abb19ccba63c84c5f259d5
Ridouaneoumohamed <ridouaneoumohamed2@gmail.com>
2024-11-09 15:27:00 +0100

    first commit

---
+++ src/main/java/com/example/touriste/TouristeApplication.java
@@ -0,0 +1,13 @@
+package com.example.touriste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/touriste/TouristeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touriste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
